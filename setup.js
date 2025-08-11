6c34a43e31eb2d453ddb54ad8398d4c9859ecc84
Jack Fredericksen <jack.a.fredericksen@gmail.com>
2025-08-11 00:15:54 -0400

    77

--- setup.js
+++ setup.js
@@ -4,7 +4,7 @@ import { Network, Alchemy } from "alchemy-sdk";
 
 // Optional config object, but defaults to demo api-key and eth-mainnet.
 const settings = {
-  apiKey: "pgBNejsZ2-oxWyMJwsshM", // Replace with your Alchemy API Key.
+  apiKey: , // Replace with your Alchemy API Key.
   network: Network.ETH_MAINNET, // Replace with your network.
 };
 const alchemy = new Alchemy(settings);
